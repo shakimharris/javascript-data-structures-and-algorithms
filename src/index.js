84a50e2effa7aa83231b272b3b5cbd3e309e7945
Shakim Harris <amenraprod@gmail.com>
2019-11-07 21:52:10 -0500

    added secondset

--- src/index.js
+++ src/index.js
@@ -107,14 +107,12 @@ function mySet() {
     return collection.length;
   };
   // this function will return the combination of the two sets
-  this.union = function(otherSet){
-   var union = new Set(); 
-   var firstSet = this.values();
-   var secondSet = otherSet.values();
-   firstSet.forEach(function(e)){
-     if(otherSet.has(e)){
-       return 
-     }
-   }
-  }
+  this.union = function(otherSet) {
+    var union = new mySet();
+    var firstSet = this.values();
+    var secondSet = otherSet.values();
+    firstSet.forEach(function(e) {
+      unionSet.add(e);
+    });
+  };
 }
