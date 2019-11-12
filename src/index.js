eb489927aa5149fcba4f28560bb5a6a1f93e64d7
Shakim Harris <amenraprod@gmail.com>
2019-11-12 17:53:01 -0500

    added search function

--- src/index.js
+++ src/index.js
@@ -20,4 +20,20 @@ class BST {
   constructor() {
     this.root = null;
   }
+  add(data){
+    const node = this.root;
+    if(node === null){
+      this.root = new Node(data);
+      return;
+    } else 
+    const searchTree = function(node) {
+      if (data < node.data){
+       if (node.left === null) {
+        node.left = new Node(data);
+        return;
+       }
+      }
+    }
+  }
+}
 }
