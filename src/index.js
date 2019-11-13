1b9fb790176c2f520eb97b880f902fab0cf3db1a
Shakim Harris <amenraprod@gmail.com>
2019-11-12 19:24:39 -0500

    setup hash function

--- src/index.js
+++ src/index.js
@@ -7,111 +7,12 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
-//  create Binary Search Tree
+//  create Hash table
 
-class Node {
-  constructor(data, left = null, right = null) {
-    this.data = data;
-    this.left = left;
-    this.right = right;
+var hash = (string, max) => {
+  var hash = 0;
+  for (var i = 0; i < string.length; i++) {
+    hash += string.charcodeAt(i);
   }
-}
-class BST {
-  constructor() {
-    this.root = null;
-  }
-  add(data){
-    const node = this.root;
-    if(node === null){
-      this.root = new Node(data);
-      return;
-    } else 
-    const searchTree = function(node) {
-      if (data < node.data){
-       if (node.left === null) {
-        node.left = new Node(data);
-        return;
-       } else if (node.left !=== null){
-         return searchTree(node.left);
-       }
-      } else if (node.right === null) {
-       node.right = new Node(data);
-      } else if (node.right !== null) {
-        return searchTree(node.right);
-      } else {
-        return null;
-      }
-      return searchTree(node);
-    }
-  }
-}
-}
-findMin (){
-  let current = this.root;
-  while (current.right !== null){
-    current = current.data;
-  }
-  return current.data;
-}
-findMax() {
-  let current = this.root;
-  while (current.left !== null){
-    current = current.data;
-  }
-  return current.data;
-}
-find(data){
-  let current = this.root;
-  while (current.data !== data){
-    if(data < current.data){
-      current = current.left;
-    } else {
-      if(data > current.data){
-        current = current.right;
-      }
-    }
-  }
-  return current;
-}
-isPresent(data){
-  let current = current.data;
-  while (current){
-    if(current === current.data){
-      return true;
-    }
-  }
-  if (data < current.data){
-    current = current.left;
-  } else 
-   current = current.right;
-}
-}
-return false;
-
-remove(data){
-  const removeNode = function(node, data){
-    if (node === null){
-      return null;
-    }
-    if (data == node.data){
-      //node has no children
-
-
-    }
-    if (node.right = null && node.left = null){
-      return null;
-    }
-  }
-  if (node.left == null){
-    return node.right;
-  }
-  if (node.right == null) {
-    return node.left; 
-  }
-  var tempNode = node.right;
-  while (tempNode.left !== null){
-    tempNode = node.left;
-  }
-  node.data = tempNode.data;
-  node.right = removeNode(node.right, tempNode.data);
-}
+  return hash % max;
+};
